416b1d1a276b26d766b866ea19edc48e041fef1e
deekshithnarayan016 <deekshithnarayan016@gmail.com>
2026-06-04 09:44:42 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn import tree
+
+iris = load_iris()
+
+X = iris.data
+y = iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=42
+)
+
+model = DecisionTreeClassifier(max_depth=3)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
+# Visualization
+plt.figure(figsize=(15,10))
+tree.plot_tree(
+    model,
+    feature_names=iris.feature_names,
+    class_names=iris.target_names,
+    filled=True,
+    rounded=True
+)
+
+plt.title("Decision Tree")
+plt.show()
